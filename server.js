529493ac2c82cef8c451914951b4964b72ea8638
wellington moreira <wsantos08@hotmail.com>
2024-06-05 08:55:40 -0300

    feat create

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize, DataTypes } from 'sequelize'
 
 const app = express();
 const port = 3333;
@@ -13,17 +13,41 @@ app.use(express.json())
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: "db/database.sqlite"
-})
+});
 
-const employee = sequelize.define("employee",{
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cargo: DataTypes.STRING,
-    status:DataTypes.BOOLEAN,
-})
+const Employee = sequelize.define("Employee", {
+    nome : DataTypes.STRING,
+    email : DataTypes.STRING,
+    cargo : DataTypes.STRING,
+    status : DataTypes.BOOLEAN,
+});
 
 app.get('/test', (req, res) => {
     res.send('api rodando')
+});
+
+app.post('/colaboradores', async (req,res) => {
+    const {nome, email, cargo, status} = req.body;
+
+    if(!nome || !email){
+        return res.send(`<div><p>Preencha todos os campos!</p></div>`)
+    }
+    
+    try{
+        const funcionario = await Employee.create({
+            nome,
+            email,
+            cargo,
+            status: status ? true : false
+        })
+        return res.send(`<div><p>Colaborador cadastrado com sucesso!</p></div>`);
+
+    }catch(err){
+        console.log(err);
+        return res.send(`<div><p>Erro ao registrar colaborador</p></div>`);
+    }
+
+    res.send('its works');
 })
 
 sequelize.sync().then(() => {
