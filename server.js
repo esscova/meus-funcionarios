d8d911a3f19c0e657ff5f1b363e419ae72b1ecab
wellington moreira <wsantos08@hotmail.com>
2024-06-11 10:07:29 -0300

    feat: listagem de funcionarios

--- server.js
+++ server.js
@@ -52,6 +52,31 @@ app.post('/colaboradores', async (req,res) => {
     res.send('its works');
 })
 
+app.get('/colaboradores', async (req, res)=>{
+    try {
+        const funcionarios = await Employee.findAll();
+        let html = funcionarios.map((funcionario)=> `
+            <div style='background-color:#fafafa; padding:8px; border-radius:8px; margin-bottom:14px; position:relative' >
+                <strong style='color:#000; font-size:20px;'>${funcionario.nome}</strong>
+                <p style='color:#000'> Email: ${funcionario.email}</p>
+                <p style='color:#000'> Cargo: ${funcionario.cargo}</p>
+                <p style='color:#000'> Status: ${funcionario.status ? "<span style='background-color:#039e00; padding:0px 8px;'> Ativo </span>":"<span style='background-color:#bf0d02; padding:0px 8px; color:#fff'> Inativo </span>"}</p>
+
+                <div style='position:absolute; top:14px; right:14px'>
+                    <button style='background-color:#121212; padding:0 8px; color:#fff'>Editar</button>
+                    <button style='background-color:#ef4444; padding:0 8px; color:#fff'>Deletar</button>
+                </div>
+            </div>
+        `).join('')
+
+        return res.send(html)
+
+    } catch (error) {
+        console.log(error);
+        return res.send(`<div style='background-color: rgba(255,102,102,0.8); position: absolute; top:24px; right: 24px; padding: 4px 24px; border-radius:4px'><p style='color:#fff'>Erro ao buscar dados...</p></div>`);
+    }
+})
+
 sequelize.sync().then(() => {
     app.listen(port, () => {
         console.log(`Server Online - url http://localhost:${port}`)
