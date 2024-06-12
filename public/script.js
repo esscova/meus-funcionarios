fdf00ede45020b7e54b7f13d319378d0fb701408
wellington moreira <wsantos08@hotmail.com>
2024-06-12 17:29:57 -0300

    atualizando lista em tempo real e limpando campos do formulario

--- public/script.js
+++ public/script.js
@@ -1,8 +1,19 @@
+document.addEventListener('htmx:afterRequest', async function(event){
+    if(event.target.getAttribute('id') === 'form-funcionarios'){
+        await fetchFuncionarios();
+        document.querySelector('#form-funcionarios').reset();
+    }
+})
+
+async function fetchFuncionarios(){
+    await htmx.ajax('GET', 'http://localhost:3333/colaboradores', '#lista-funcionarios')
+}
+
 document.addEventListener('htmx:configRequest', function(event){
     // console.log(event);
     if(event.detail.path === '/colaboradores' && event.detail.verb !== 'get'){
         setTimeout(()=>{
             document.querySelector('#toast').innerHTML = '';
-        }, 4000)
+        }, 4000);
     }
-})
+});
